5484294ccfb4748a85f3a2f9b4aa541a1164dc91
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-03 13:58:03 +0100

    Modifica models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,10 +8,13 @@ use Illuminate\Support\Str;
 
 class Project extends Model
 {
-    protected $fillable=['title', 'content', 'slug','date_project'];
+    protected $fillable=['title', 'content', 'slug','date_project','type_id'];
     use HasFactory;
 
     public static function generateSlug($title){
         return Str::slug($title, '-');
+    }
+    public function type(){
+        return $this->belongsTo(Type::class);
     }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Project;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+    protected $fillable= ['name', 'slug'];
+
+    public function projects(){
+        return $this->hasMany(Project::class);
+    }
 }
