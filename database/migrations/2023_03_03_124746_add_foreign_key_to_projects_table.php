96c6c729d90137954b3b00c205461dde35f65665
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-03-03 13:54:31 +0100

    Update: -Creato controller; -Fatte le migrations; -Lanciato il seeder.

---
+++ database/migrations/2023_03_03_124746_add_foreign_key_to_projects_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //Creiamo la colonna per la foeing key
+            $table->unsignedBigInteger('type_id') //coincidere
+                ->nullable()
+                ->after('id');
+            //creo la foreign key
+            $table->foreign('type_id')//coincidere
+                ->references('id') //nome colonna di riferimento
+                ->on('types') //quale tabella appartiene
+                ->onDelete('set null'); //diciamo esplicitamente di settare a null la colonna nel caso in cui cancellassimo la categoria
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            //passsaggio
+            $table->dropForeign('projects_type_id_foreign'); //droppa nella tabella projects la colonna category_id che è la foreign key
+            //2 passaggio
+            $table->dropColumn('type_id');
+        });
+    }
+};
